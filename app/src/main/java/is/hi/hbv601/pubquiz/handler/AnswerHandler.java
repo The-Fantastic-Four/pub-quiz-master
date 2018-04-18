e5a25ba1924ebb56c72419ed428aa378e11d2273
Viktor Alex <viktoralex@365.is>
2018-04-18 16:55:27 +0000

    Removed firebase dependencies for question and review fragments

--- app/src/main/java/is/hi/hbv601/pubquiz/handler/AnswerHandler.java
+++ app/src/main/java/is/hi/hbv601/pubquiz/handler/AnswerHandler.java
@@ -7,16 +7,42 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import is.hi.hbv601.pubquiz.fragment.AnswerListenerFragment;
+import is.hi.hbv601.pubquiz.model.QuizHolder;
 
 public class AnswerHandler {
     private AnswerListenerFragment fragment;
 
+    private String questionId;
+    private String quizId;
+    private String teamName;
+
     public AnswerHandler(String questionId, String quizId, String teamName, AnswerListenerFragment fragment)
     {
         this.fragment = fragment;
+        this.questionId = questionId;
+        this.quizId = quizId;
+        this.teamName = teamName;
 
         DatabaseReference answerRef = FirebaseDatabase.getInstance().getReference("answers/" + quizId + "/" + questionId + "/" + teamName + "/");
         answerRef.addValueEventListener(new AnswerListener());
+    }
+
+    public void setAnswer(String answer)
+    {
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference(
+                "answers/" + this.quizId + "/" + this.questionId + "/" + this.teamName + "/answer");
+
+        // Save the answer to the db
+        mDatabase.setValue(answer);
+    }
+
+    public void setAnswerIsCorrect(Boolean isCorrect)
+    {
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference(
+                "answers/" + this.quizId + "/" + this.questionId + "/" + this.teamName + "/isCorrect");
+
+        // Save the answer to the db
+        mDatabase.setValue(isCorrect);
     }
 
     private class AnswerListener implements ValueEventListener {
